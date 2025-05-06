6a7901d1ee8bcfac98341c039e49ccd2ee37a41f
rohanlahar <rohan.laharwani@gmail.com>
2025-05-06 15:55:57 +0530

    Create EXP 2.py

---
+++ EXP 2.py
@@ -0,0 +1,7 @@
+#You are given a length L and breadth B and your task is to find the Area of the Rectangle.
+
+L=int(input("L:"))
+B=int(input("B:"))
+# Make use of the values of L and B read using the input function.
+Area = L * B
+print(Area)
